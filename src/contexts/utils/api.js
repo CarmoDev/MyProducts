8329388b490abdd00a3223e31e1e540cf57445c2
JeffersonDevWeb <dalvatechnologies@gmail.com>
2023-05-31 20:35:40 -0300

    update

--- src/contexts/utils/api.js
+++ src/contexts/utils/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: 'https://myproductsbackend.glitch.me/',
 });
